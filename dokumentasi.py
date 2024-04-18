251ef5036e0129ae0ac7a020f7aca019a14a805b
gerardusdavidbayuaji <gerardusdavidbayu@gmail.com>
2024-04-19 05:40:31 +0700

    feat: tipe data

---
+++ dokumentasi.py
@@ -0,0 +1,28 @@
+# __VARIABEL__ 
+nama_a = "udin"
+print(nama_a)
+
+# __TIPE DATA__
+# tipe data standar: integer, float, string, boolean
+
+# tipe data integer: ga ada koma
+data_int = 10
+print("data:", data_int)
+print("bertipe:", type(data_int))
+
+# tipe data float: ada koma
+data_flt = 10.5
+print("data:", data_flt)
+print("bertipe:", type(data_flt))
+
+# tipe data string: kumpulan karakter
+data_str = 'gelas'
+print("data:", data_str)
+print("bertipe:", type(data_str))
+
+# tipe data boolean: biner 0/1 (true/false)
+data_bool = True
+print("data:", data_bool)
+print("bertipe:", type(data_bool))
+
+# __CASTING__
