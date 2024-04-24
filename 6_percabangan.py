c41e4797de581b671c8b07e64748c0dca554d22f
Gerardusdavid <gerardusdavidbayu@gmail.com>
2024-04-24 16:48:03 +0700

    feat: percabangan

---
+++ 6_percabangan.py
@@ -0,0 +1,72 @@
+# A1. IF ELSE STATEMENT
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+# namaUser = input('Siapa nama kakak? ')
+
+# alur programmnya
+# if kondisi: aksi
+# program selanjutnya
+
+# program if inline
+# if namaUser == 'budi': print(f'hallo {namaUser}, mau pesan apa?')
+# print(f'terima kasih {namaUser}')
+
+# program if identasi & if statement
+# if namaUser == 'john': #aksi
+#     print(f'hallo {namaUser}, mau pesan apa?') #kondisi & identasi
+# else: #kondisi & identasi
+#     print('sampai jumpa lagi') #kondisi & identasi
+# print(f'terima kasih {namaUser}') #end
+
+# A2. ELIF STATEMENT
+# elif = else if statement
+
+# nama = input('masukan nama anda ')
+
+# if kondisi:
+#     aksi True 1
+# elif kondisi:
+#     aksi True 2, dst bisa buat elif sebanyak mungkin
+# else:
+#     aksi
+
+# if nama == 'budi':
+#     print('berhasil login')
+# elif nama == 'john':
+#     print('berhasil login')
+# else:
+#     print('opps something when wrong')
+
+# LATIHAN PROGRAM KALKULATOR
+
+# angka_pertama = float(input('masukan nilai pertama :'))
+# operator = input('masukan operator x, + , /, - disini :')
+# angka_kedua = float(input('masukan nilai pertama :'))
+
+# if operator == 'x' or operator == '*':
+#     hasil = angka_pertama * angka_kedua
+#     print('hasilnya adalah :', hasil)
+# elif operator == '+':
+#     hasil = angka_pertama + angka_kedua
+#     print('hasilnya adalah :', hasil)
+# elif operator == '/':
+#     hasil = angka_pertama / angka_kedua
+#     print('hasilnya adalah :', hasil)
+# elif operator == '-':
+#     hasil = angka_pertama - angka_kedua
+#     print('hasilnya adalah :', hasil)
+# else:
+#     print('aelahh... masukin nilai yang bener broo..')
+
+# LATIHAN CHAT
+# nama = input('masukan nama anda :')
+
+# if nama == 'budi':
+#     input(f'hallo {nama}, apa kabar? ')
+#     print('okee, selamat bekerja kembali...')
+# else:
+#     print('opps something when wrong..')
+
--- 6_program_jadwal.py
+++
@@ -1,5 +0,0 @@
-import datetime as dt
-
-hari_ini = dt.date.today()
-
-print(hari_ini)
