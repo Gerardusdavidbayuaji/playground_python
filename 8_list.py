a2674e88ec41c3c006083e483ce500300b4d5d9c
Gerardusdavid <gerardusdavidbayu@gmail.com>
2024-04-26 11:56:56 +0700

    feat: add list

--- 8_list.py
+++ 8_list.py
@@ -63,3 +63,18 @@ print('ini adalah data gabungan :', number_list)
 # menghapus data pada list
 number_list.remove('otong')
 print('ini data yang dihapus :', number_list)
+
+# __NESTED LIST__
+
+peserta_0 = ['budi', 23, 'laki-laki']
+peserta_1 = ['ani', 25, 'perempuan']
+peserta_2 = ['putri', 27, 'perempuan']
+
+list_peserta = [peserta_0, peserta_1, peserta_2]
+
+print(f'{list_peserta}')
+
+for peserta in list_peserta:
+    print(f'nama\t {peserta[0]}')
+    print(f'umur\t {peserta[1]}')
+    print(f'gender\t {peserta[2]}\n')
